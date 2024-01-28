63f3682907d21e16eebfd2d12d3f6d3c6ae6f2d1
Kubsy <kubadd475@gmail.com>
2024-01-28 14:13:23 +0000

    Implement the rest of functions

--- Music-Player/MainWindow.xaml.cs
+++ Music-Player/MainWindow.xaml.cs
@@ -1,6 +1,9 @@
-﻿using System;
+﻿using Microsoft.Win32;
+using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Media;
+using System.Runtime.CompilerServices;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
@@ -20,9 +23,45 @@ namespace Music_Player
     /// </summary>
     public partial class MainWindow : Window
     {
+
+        private MediaPlayer player;
+        private TimeSpan trackPos;
         public MainWindow()
         {
             InitializeComponent();
+            player = new MediaPlayer();
+        }
+
+        private void loadFile(object sender, RoutedEventArgs e)
+        {
+            OpenFileDialog openFile = new OpenFileDialog();
+
+            openFile.Filter = "Audio files (*.wav)|*.wav|All files (*.*)|*.*";
+
+            if (openFile.ShowDialog() == true)
+            {
+                Filename.Text = openFile.SafeFileName;
+                player.Open(new Uri(openFile.FileName));
+            }
+        }
+
+        private void playTrack(object sender, RoutedEventArgs e)
+        {
+            player.Play();
+        }
+
+        private void stopTrack(object sender, RoutedEventArgs e)
+        {
+        }
+
+        private void pauseTrack(object sender, RoutedEventArgs e)
+        {
+            player.Pause();
+        }
+
+        private void Exit(object sender, RoutedEventArgs e)
+        {
+            this.Close();
         }
     }
 }
